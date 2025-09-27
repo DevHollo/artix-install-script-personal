797999c437068fdc910a0f5173391205da814a54
Randall Winkhart <idgr@tutanota.com>
2025-09-27 13:53:24 -0400

    Add simple "unmv" (undo mv command) alias

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 loadkeys us
 echo ----------------------------------------------------------------------------------------------
 echo rwinkhart\'s Artix Install Script
-echo Last updated May 29, 2025 \(rev. A\)
+echo Last updated September 27, 2025 \(rev. A\)
 echo ----------------------------------------------------------------------------------------------
 echo You will be asked some questions before installation.
 echo -e "----------------------------------------------------------------------------------------------\n"
